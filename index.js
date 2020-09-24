ff5efbcab8b3f26a3b56e5cbe484da3f5814e781
kholah-salamah <kholahali465@gmail.com>
2020-09-25 01:31:36 +0800

    2 commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const config = require("config");
 const app = express();
 console.log("github");
+
+console.log("github2commit");
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
